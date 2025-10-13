d9474238d5fe3ed6f35252d6d4849cfc482ab675
chinky23-tech <chinky0330@gmail.com>
2025-10-14 00:07:09 +0530

    about page

--- server.js
+++ server.js
@@ -6,6 +6,18 @@ const app = express();
 // Serve static files from "public" folder
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.listen(3030, () => {
-  console.log('✅ Server running at http://localhost:3030');
+//add  custom route for about page
+app.get('/about' ,(req , res) => {
+res.sendFile(path.join(__dirname, 'public' , 'about.html'));
+});
+
+app.get('/api/info' , (req,res) => {
+res.json({
+    name: "My Express App",
+    version: "1.0.0",
+    message: "Hello from JSON route!"
+});
+});
+app.listen(5050, () => {
+  console.log('✅ Server running at http://localhost:5050');
 });
